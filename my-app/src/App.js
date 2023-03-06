20dc0c3f6f1229b1a31e541d38bb2734241ac115
SudeepAcharjee <sudeepacharjeegp@gmail.com>
2023-03-06 12:39:52 +0530

    React#1

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import hello from './hello.js';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello World</h1>
+        <img src={logo} className="App-logo" alt="logo" />
+      <p>@SudeepAcharjee</p>
+<home/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
